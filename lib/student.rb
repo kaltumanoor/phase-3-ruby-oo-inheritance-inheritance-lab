a5154ee088efc1e6b9948ddb71898ae79d189db9
kaltuma <kaltumanoor370@gmail.com>
2023-02-19 20:16:31 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    attr_reader :knowledge
+
+
+    def initialize
+        @knowledge = []
+
+    end
+
+    def learn(subs)
+        @knowledge << subs
+
+    end
 
 end
+
+Kaltuma = Student.new
+Kaltuma.first_name=("loop")
+p Kaltuma.first_name
